1b6b4fdaf6ab4d58529e7f8e2d5cbc2faec7581c
yafa@expertbook <yafa.expertbook@gmail.com>
2026-06-22 15:15:02 +0700

    penyesuaian judul

--- resources/views/login/v_index.blade.php
+++ resources/views/login/v_index.blade.php
@@ -5,7 +5,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>TemanGenerus | Sragen Barat - Sistem Administrasi Terintegrasi untuk Generus</title>
+    <title>TemanGenerus | Solo Selatan - Sistem Administrasi Terintegrasi untuk Generus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Sistem Administrasi Terintegrasi untuk Generus" name="description" />
     <meta content="ManekaromaTeknologi" name="author" />
@@ -44,7 +44,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-center mt-sm-5 mb-4 text-white">
-                            <span class="fw-bold fs-3">TemanGenerus X PPG Sragen Barat</span>
+                            <span class="fw-bold fs-3">TemanGenerus X PPG Solo Selatan</span>
                             <p class="mt-3 fs-15 fw-medium text-white-50">
                                 Sistem Manajemen Generus Terintegrasi untuk presensi, kegiatan rutin,
                                 monitoring perkembangan, dan administrasi generus.
